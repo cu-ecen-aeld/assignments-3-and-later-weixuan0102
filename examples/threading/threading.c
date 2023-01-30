ed87bc064800dd295472e819b779c58adf23d3d9
weixuan0102 <leewe2014@gmail.com>
2023-01-30 12:26:23 +0800

    assignment 4 part 1 completed.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -13,7 +15,21 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = true;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    DEBUG_LOG("lock");
+    if(pthread_mutex_lock(thread_func_args->mutex) != 0){
+	    ERROR_LOG("Can't lock mutex");
+	    thread_func_args->thread_complete_success = false;
+    }
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    if(pthread_mutex_unlock(thread_func_args->mutex) != 0){
+	    thread_func_args->thread_complete_success = false;
+	    ERROR_LOG("Can't unlock mutex");  
+    }
+    DEBUG_LOG("unlock");
+	
     return thread_param;
 }
 
@@ -28,6 +44,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* tData = malloc(sizeof(struct thread_data));
+    if(tData == NULL){
+	    ERROR_LOG("Memory allocation failure for creating data");	
+	    return false;
+    }
+    /*
+    if(pthread_mutex_init(mutex, NULL) != 0){
+	    ERROR_LOG("mutex initialization failed.");
+	    return false;
+    }
+    */
+    tData->wait_to_obtain_ms = wait_to_obtain_ms;
+    tData->wait_to_release_ms = wait_to_release_ms;
+    tData->mutex = mutex;
+    int rc = pthread_create(thread, NULL, threadfunc, tData); 
+    if(rc != 0){
+	    ERROR_LOG("Can't create thread");
+	    return false;
+    }
+
+    return true;
 }
 
